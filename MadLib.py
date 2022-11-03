88f3326a601aee90a95ee831190830aeb1c541a6
0gl04q <115027096+0gl04q@users.noreply.github.com>
2022-11-03 12:51:26 +0300

    change MadLib, add all rand word

--- MadLib.py
+++ MadLib.py
@@ -3,6 +3,7 @@ import random
 
 
 def form_game():
+    # Графический интерфейс
     layout_column = [
             [sg.Text('Генератор MadLibs', justification='center', font=("Verdana", 20, "bold"))],
             [sg.Text('[name] - Данил, Сергей, Петр, ', font=("Verdana", 8)), sg.InputText(key='name', size=(42, 1))],
@@ -12,35 +13,43 @@ def form_game():
             [sg.Submit('Выполнить'), sg.Cancel('Отмена')]
     ]
 
+    # Установка позиции элементов
     layout = [[sg.Column(layout_column, element_justification='center')]]
 
-    window = sg.Window('My new window', layout, grab_anywhere=True)
+    # Инициализация объекта окна
+    window = sg.Window('MadLibs', layout, grab_anywhere=True)
 
+    # Цикл работы графического интерфейса
     while True:
         event, values = window.read()
         if event == 'Выполнить':
-            submit_button(values)
-        if event in (None, 'Exit', 'Отмена'):
-            break
+            window.Element("text").Update(submit_button(values))  # Вызов действия кнопки выполнить
+        if event in (None, 'Exit', 'Отмена'):  # Выход из цикла
+            break  # Закрытие программы
 
 
-def submit_button(vals):
+def submit_button(vals):  # Действие кнопки выполнить
+    # Начальный список
     sp = {
         'name': ['Данил', 'Сергей', 'Петр'],
         'professional': ['Программист', 'Бульдозерист', 'Хакер'],
         'friend': ['Витя', 'Саня', 'Коля'],
     }
 
+    # Получение текста
     text = vals['text']
+
+    # Добавление новых позиций в список
     for val in vals:
         if vals[val] != '' and val != 'text':
             sp[val] = sp[val] + vals[val].split(',')
-        if val == 'text':
-            for val_1 in vals:
-                if val_1 != 'text':
-                    text = text.replace(f'[{val_1}]', random.choice(sp[f'{val_1}']).strip())
 
-    print(text)
+    # Обход списка со смешными словами
+    for val in sp:
+        while text.find(f'[{val}]') != -1:  # Цикл для вставки случайных слов на каждую вставку
+            text = text.replace(f'[{val}]', random.choice(sp[f'{val}']), 1)
+
+    return text
 
 
 if __name__ == '__main__':
